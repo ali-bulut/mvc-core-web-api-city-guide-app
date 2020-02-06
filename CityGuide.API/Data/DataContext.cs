1b4f22c5e078bd1f92bcabf3a159d0d32151e672
Ali Bulut <alibulut@yahoo.com>
2020-02-07 00:10:52 +0300

    angular and mvc core web api projects were created

---
+++ CityGuide.API/Data/DataContext.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using CityGuide.API.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace CityGuide.API.Data
+{
+    public class DataContext:DbContext
+    {
+        //Database'i bağlama
+        public DataContext(DbContextOptions<DataContext> options):base(options)
+        {
+            
+        }
+        //Database'deki tabloları bağlama
+        public DbSet<Value> Values { get; set; }
+    }
+}
